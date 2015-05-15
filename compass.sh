6f364eebca20ad66352941df4f282a98d261609a
Dennis Schwertel <d.schwertel@s-v.de>
2015-05-15 14:36:05 +0200

    indentation

--- compass.sh
+++ compass.sh
@@ -11,19 +11,19 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 touch .checkpoint
 while true; do
-	if [[ -n $(find $SCSS_FOLDER -newer .checkpoint) ]]; then
-		touch .checkpoint
-		echo "+"
-		notify-send "compile: START"
-		date1=$(date +"%s")
-		#hack because vagrant ssh -c does not work properly
-		vagrant ssh-config > .vagrant-ssh
-	    ssh -F .vagrant-ssh default  $COMMAND
-	    date2=$(date +"%s")
-		diff=$(($date2-$date1))
-		notify-send -i "${DIR}/success.png" "compile: DONE in ${diff}s "
-	else
-    	echo -n "."
-	fi
-	sleep 1
+    if [[ -n $(find $SCSS_FOLDER -newer .checkpoint) ]]; then
+        touch .checkpoint
+        echo "+"
+        notify-send "compile: START"
+        date1=$(date +"%s")
+        #hack because vagrant ssh -c does not work properly
+        vagrant ssh-config > .vagrant-ssh
+        ssh -F .vagrant-ssh default  $COMMAND
+        date2=$(date +"%s")
+        diff=$(($date2-$date1))
+        notify-send -i "${DIR}/success.png" "compile: DONE in ${diff}s "
+    else
+        echo -n "."
+    fi
+    sleep 1
 done
